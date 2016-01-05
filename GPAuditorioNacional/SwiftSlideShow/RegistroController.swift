42df706a6e1e7bf12ce3cc25517b640bf7bc2f31
Hector Aguilar <hectoraguilar.adesis@gmail.com>
2016-01-05 02:07:52 -0600

    Cambios en el formulario

--- GPAuditorioNacional/SwiftSlideShow/RegistroController.swift
+++ GPAuditorioNacional/SwiftSlideShow/RegistroController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class RegistroController: UIViewController {
 
+    @IBOutlet weak var nombreValue: UITextField!
+    @IBOutlet weak var celularValue: UITextField!
+    @IBOutlet weak var emailValue: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,6 +28,37 @@ class RegistroController: UIViewController {
     }
     
 
+    @IBAction func crearRegistro(sender: AnyObject) {
+        
+        let nombreField = nombreValue.text
+        let emailField = emailValue.text
+        
+        
+        
+        let request = NSMutableURLRequest(URL: NSURL(string: "http://emocionganar.com/admin/panel/registro_ios.php")!)
+            request.HTTPMethod = "POST"
+            let postString = "email=\(emailField)&nombre=\(nombreField)"
+            request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding)
+            let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { data, response, error in
+                guard error == nil && data != nil else {                                                          // check for fundamental networking error
+                    print("error=\(error)")
+                    return
+                }
+            
+                if let httpStatus = response as? NSHTTPURLResponse where httpStatus.statusCode != 200 {           // check for http errors
+                    print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                    print("response = \(response)")
+                }
+            
+                let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
+                print("responseString = \(responseString)")
+            }
+            task.resume()
+        
+
+        
+        
+    }
     /*
     // MARK: - Navigation
 
