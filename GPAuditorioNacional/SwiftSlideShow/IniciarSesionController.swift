27adc2799754c4632e9f6280dc71f6ee53776c1a
Hector Aguilar <hectoraguilar.adesis@gmail.com>
2016-01-06 02:06:12 -0600

    Cambios

--- GPAuditorioNacional/SwiftSlideShow/IniciarSesionController.swift
+++ GPAuditorioNacional/SwiftSlideShow/IniciarSesionController.swift
@@ -25,6 +25,67 @@ class IniciarSesionController: UIViewController {
     }
     
     @IBAction func iniciarSesion(sender: UIButton) {
+
+        let request = NSMutableURLRequest(URL: NSURL(string: "http://emocionganar.com/admin/panel/registro_ios.php")!)
+        request.HTTPMethod = "POST"
+        let postString = "email=\(emailField!)&nombre=\(nombreField!)&celular=\(celularField!)&telefono=\(telefonoField!)&codigo_postal=\(codigoPostalField!)&edad=\(edadField!)&username=\(usernameField!)&password=\(passwordField!)"
+        request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding)
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { data, response, error in
+            guard error == nil && data != nil else {                                                          // check for fundamental networking error
+                print("error=\(error)")
+                return
+            }
+            
+            if let httpStatus = response as? NSHTTPURLResponse where httpStatus.statusCode != 200 {           // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(response)")
+            }
+            
+            let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
+            
+            print("responseString Esto imprime = \(responseString!)")
+            
+            
+            if(responseString! == "success"){
+                print("Debe de continuar la app")
+                
+                
+                
+                /*
+                NSOperationQueue.mainQueue().addOperationWithBlock {
+                self.performSegueWithIdentifier("registroExitoso", sender: self)
+                }
+                */
+                
+                let nuestroStoryBoard: UIStoryboard = UIStoryboard(name:"Main", bundle:nil)
+                let registroExitosoPantalla = nuestroStoryBoard.instantiateViewControllerWithIdentifier("registroExitoso") as! RegistroExitosoController
+                
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.navigationController!.pushViewController(registroExitosoPantalla, animated:true)
+                })
+                
+            }
+            if(responseString! == "Registro duplicado"){
+                print("El registro ya esta registrado")
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.avisoLabel.text = "El usuario ya ha sido registrado"
+                })
+                
+            }
+            if(responseString! == "false"){
+                
+                print("Debe de continuar la app")
+                
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.avisoLabel.text = "Es necesario llenar todos los campos."
+                })
+                
+                
+            }
+            
+        }
+        task.resume()
+        
         
     }
 
