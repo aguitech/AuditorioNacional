7c76127488db341a769a022fad859395713cd14b
Héctor Aguilar <hectoraguilar@MBPro-HA-3.local>
2016-01-07 16:29:52 -0600

    Cambios en app

--- GPAuditorioNacional/SwiftSlideShow/RegistroController.swift
+++ GPAuditorioNacional/SwiftSlideShow/RegistroController.swift
@@ -25,9 +25,17 @@ class RegistroController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
         // Do any additional setup after loading the view.
     }
+    
+    //Calls this function when the tap is recognized.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
