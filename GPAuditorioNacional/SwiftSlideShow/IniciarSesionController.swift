123659b687f31187e02774a84904b65ea124f0d7
Hector Aguilar <hectoraguilar.adesis@gmail.com>
2016-01-26 20:26:02 -0600

    Cambios en inicio de sesion

--- GPAuditorioNacional/SwiftSlideShow/IniciarSesionController.swift
+++ GPAuditorioNacional/SwiftSlideShow/IniciarSesionController.swift
@@ -31,75 +31,58 @@ class IniciarSesionController: UIViewController {
     
     @IBAction func iniciarSesion(sender: UIButton) {
 
+        enum JSONError: String, ErrorType {
+            case NoData = "ERROR: no data"
+            case ConversionFailed = "ERROR: conversion from JSON failed"
+        }
+        
         let emailField = emailValue.text
         let passwordField = passwordValue.text
         
-        let request = NSMutableURLRequest(URL: NSURL(string: "http://emocionganar.com/admin/panel/login_ios.php")!)
+        let request = NSMutableURLRequest(URL: NSURL(string: "http://emocionganar.com/admin/panel/login_ios_new.php")!)
         request.HTTPMethod = "POST"
         let postString = "email=\(emailField!)&password=\(passwordField!)"
         request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding)
-        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { data, response, error in
-            guard error == nil && data != nil else {                                                          // check for fundamental networking error
-                print("error=\(error)")
-                return
-            }
-            
-            if let httpStatus = response as? NSHTTPURLResponse where httpStatus.statusCode != 200 {           // check for http errors
-                print("statusCode should be 200, but is \(httpStatus.statusCode)")
-                print("response = \(response)")
-            }
-            
-            let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
-            
-            print("responseString Esto imprime = \(responseString!)")
-            
-            
-            if(responseString! == "success"){
-                print("Debe de continuar la app")
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { (data, response, error) -> Void in
+            do {
+                guard let dat = data else { throw JSONError.NoData }
+                guard let json = try NSJSONSerialization.JSONObjectWithData(dat, options: []) as? NSDictionary else { throw JSONError.ConversionFailed }
+                //print(json)
+                print(json["success"]!)
+                //print(json["result"])
+                //print(json["result"]!)
+                print(json["result"]![0])
+                print(json["result"]![1]!["id"]!)
+                print(json["result"]![2]["nombre"]!)
+                print(json["result"]![2]["nombre"])
+                print(json["result"]![2]!["nombre"])
+                print(json["result"]![2]!["nombre"]!)
                 
-                
-                
-                /*
-                NSOperationQueue.mainQueue().addOperationWithBlock {
-                self.performSegueWithIdentifier("registroExitoso", sender: self)
+                if(json["success"]! as! NSObject == 1){
+                    //Código que guarda el nombre de usuario
+                    let nombreDefault = NSUserDefaults.standardUserDefaults()
+                    nombreDefault.setValue(self.emailValue.text!, forKey: "usuario")
+                    nombreDefault.synchronize()
+                    
+                    
+                    //Código que liga a otro View
+                    let nuestroStoryBoard: UIStoryboard = UIStoryboard(name:"Main", bundle:nil)
+                    let registroExitosoPantalla = nuestroStoryBoard.instantiateViewControllerWithIdentifier("NavigationSeleccion") as! MyNavigationControllerSeleccion
+                    
+                    dispatch_async(dispatch_get_main_queue(), {
+                        self.presentViewController(registroExitosoPantalla, animated:true, completion: nil)
+                    })
+                    
+                }else{
+                    
                 }
-                */
-                print("--------------------------")
                 
                 
-                //Código que guarda el nombre de usuario
-                let nombreDefault = NSUserDefaults.standardUserDefaults()
-                nombreDefault.setValue(self.emailValue.text!, forKey: "usuario")
-                nombreDefault.synchronize()
-                
-                
-                //Código que liga a otro View
-                let nuestroStoryBoard: UIStoryboard = UIStoryboard(name:"Main", bundle:nil)
-                let registroExitosoPantalla = nuestroStoryBoard.instantiateViewControllerWithIdentifier("NavigationSeleccion") as! MyNavigationControllerSeleccion
-                
-                dispatch_async(dispatch_get_main_queue(), {
-                    self.presentViewController(registroExitosoPantalla, animated:true, completion: nil)
-                })
-                
+            } catch let error as JSONError {
+                print(error.rawValue)
+            } catch {
+                print(error)
             }
-            if(responseString! == "Registro duplicado"){
-                print("El registro ya esta registrado")
-                dispatch_async(dispatch_get_main_queue(), {
-                    //self.avisoLabel.text = "El usuario ya ha sido registrado"
-                })
-                
-            }
-            if(responseString! == "false"){
-                
-                print("Debe de continuar la app")
-                
-                dispatch_async(dispatch_get_main_queue(), {
-                    //self.avisoLabel.text = "Es necesario llenar todos los campos."
-                })
-                
-                
-            }
-            
         }
         task.resume()
         
